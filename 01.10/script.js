ee547f9441146facc454723558d696a18c445bdc
Aleksandra <milanochka3535@gmail.com>
2022-10-04 16:15:45 +0600

    feat: 01-10

---
+++ 01.10/script.js
@@ -0,0 +1,32 @@
+let a = 17;
+let b = 10;
+let c = 7;
+let d = 7;
+const result1 = a - b;
+console.log(result1)
+const result2 = c + d;
+console.log(result2)
+alert(result2)
+
+
+const hour = 'час';
+const minute =':минута';
+const second = ':секунда';
+alert(hour+minute+second);
+
+/*var num = 10;
+num = num + 1;
+num = num + 1;
+alert(num);*/
+
+let num = 11;
+let n = num++;
+console.log(n);
+console.log(num);
+alert(num);
+
+
+
+
+
+
--- введение/script.js
+++ введение/script.js
@@ -1,9 +1,9 @@
 let  camelCase = 1;
-alert('1');
+alert(camelCase);
 let snake_case = 2;
-alert('2');
+alert( snake_case);
 let PascalCase = 3;
-alert('3');
+alert( PascalCase);
 
 let name1 = 'Aleksandra';
 let name2 = 'ElsukovaAleksandra';
