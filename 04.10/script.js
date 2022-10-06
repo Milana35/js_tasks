4217e63b15ddd75b569028c335a3758d41da7e92
Aleksandra <milanochka3535@gmail.com>
2022-10-06 14:26:42 +0600

    feat:04.10

---
+++ 04.10/script.js
@@ -0,0 +1,33 @@
+let x = 10;
+x = x +1 + 1;
+let result = 12 - 1;
+console.log(x);
+console.log(result);
+
+
+let a = 47;
+console.log(a);
+a += 7;
+console.log(a);
+a -= 18;
+console.log(a);
+a *= 10;
+console.log(a);
+a /= 15;
+
+
+let num = 1;
+console.log(num);
+num += 12;
+console.log(num);
+num -= 14;
+console.log(num);
+num *= 5;
+console.log(num);
+num /= 7;
+console.log(num);
+num += 1;
+console.log(num);
+num -= 1;
+
+
