f179cf96508be95d2c6a0271c2a11feb0e2cc26d
Aleksandra <milanochka3535@gmail.com>
2022-10-13 20:07:54 +0600

    feat:11.10

---
+++ 11.10/script.js
@@ -0,0 +1,46 @@
+const userSalaries = [
+    {
+        name1: 'Ella',
+        salary: 100
+    },
+    {
+        name2: 'Sophia',
+        salary: 200
+    },
+    {
+        name3: 'Ellie',
+        salary: 300
+    }
+]
+userSalaries.pop();
+console.log(userSalaries);
+alert(userSalaries);
+
+
+/*let a = 5;
+let b = 6;
+let c = 10;
+if (a + b + c >= 21){
+    alert(21);
+}*/
+
+let sum = {
+    a: 5,
+    b: 6,
+    c: 10
+};
+alert(sum.a + sum.b + sum.c);
+
+/*let number;
+if(data === true){
+    number = 3;
+} else{
+    number = 5;
+}*/
+
+
+
+
+
+
+
